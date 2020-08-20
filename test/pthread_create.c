4fc3f002720821d83932f289bb13ade55b5881e6
xia0lei <xia0lei@DESKTOP-J7C15CO.localdomain>
2020-08-20 10:00:09 +0800

    add pthread_create.c

---
+++ test/pthread_create.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *thrd_func(void * arg){
+    printf("In thread: thread id = %lu, pid = %u\n", pthread_self(), getpid());
+    return NULL;
+}
+
+int
+main(void){
+    pthread_t tid;
+    int ret;
+
+    printf("In main 1: thread id = %lu, pid = %u\n", pthread_self(), getpid());
+    ret = pthread_create(&tid, NULL, thrd_func, NULL);
+
+    if (ret != 0){
+        printf("pthread_create error\n");
+        exit(1);
+    }
+
+
+    printf("In main 2: thread id = %lu, pid = %u\n", pthread_self(), getpid());
+    sleep(1);
+
+    return 0;
+}
